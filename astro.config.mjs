def219cf02acab87b4f884a916a2cb90bf12eab9
radenpioneer <bohrium.bearded_64@icloud.com>
2026-04-28 12:53:37 +0000

    re-init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,5 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    },
-
-    imageService: "cloudflare"
-  })
+  adapter: cloudflare()
 });
